a07c605eb3fac35b82f763da91ee707a9797720b
Oktan-Burg <80918992+Oktan-Burg@users.noreply.github.com>
2022-08-03 12:01:30 -0700

    APIs Update 0.1.6

--- src/APIs/ssh.lua
+++ src/APIs/ssh.lua
@@ -3,17 +3,13 @@ ssh.server = {}
 ssh.client = {}
  
 -- Functions --
-function ssh.server:Connect(passKeyLevel, allowedID)
+function ssh.server:Connect(channel)
     if peripheral.find("modem") == nil then error("This device must have a modem.") end
     peripheral.find("modem", rednet.open)
     while true do 
         sleep(0.4)
-        data, message = rednet.receive(50)
-        if not message == nil and data == allowedID and passKeyLevel > 0 then
-            result = shell.run(message)
-            rednet.send(data, result)
-            print("Executing: ".. message)
-        elseif not message == nil and passKeyLevel == 0 then
+        data, message = rednet.receive(channel, 50)
+        if not message == nil then
             result = shell.run(message)
             rednet.send(data, result)
             print("Executing: ".. message)
@@ -21,17 +17,17 @@ function ssh.server:Connect(passKeyLevel, allowedID)
     end
 end
 
-function ssh.client:Connect(DeviceId)
+function ssh.client:Connect(channel)
     if peripheral.find("modem") == nil then error("This device must have a modem.") end
     peripheral.find("modem", rednet.open)
     if rednet.send(DeviceId, "ping") == false then error("Error Code: 567") end
     
     local client = {}
-    client.target = DeviceId
+    client.target = channel
     function client.post(shellline)
         if rednet.isOpen() == false then peripheral.find("modem", rednet.open) end
-        rednet.send(self.target, shellline)
-        data, response = rednet.receive(30)
+        rednet.broadcast(shellline, self.target)
+        data, response = rednet.receive(self.target, 30)
         if data == DeviceId then
             if response == nil then
                 return nil
